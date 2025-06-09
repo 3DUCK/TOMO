d53be4ae9d6639f5faf31b92e1b88919d4810b9c
3DUCK <kermit7520k@gmail.com>
2025-06-09 15:44:03 +0900

    Initial Commit

---
+++ TOMO/TOMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOMOApp.swift
+//  TOMO
+//
+//  Created by KG on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TOMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
